6d701f9a45a0c783e1ed45f0412329a24b40913d
Guus Boekestein <64610574+guusgozer@users.noreply.github.com>
2025-04-07 17:46:58 +0200

    v1

---
+++ Assets/Scripts/HandleMovementCollider_ASB.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleMovementCollider_ASB : StateMachineBehaviour {
+
+    StateManager states;
+
+    public int index;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        if (states == null)
+            states = animator.transform.GetComponentInParent<StateManager>();
+
+        states.CloseMovementCollider(index);
+
+    }
+
+    
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (states == null)
+            states = animator.transform.GetComponentInParent<StateManager>();
+
+        states.OpenMovementCollider(index);
+    }
+
+}
---
+++ Assets/Scripts/Players/ChangeBool.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeBool : StateMachineBehaviour {
+
+    public string boolName;
+    public bool status;
+    public bool resetOnExit;
+
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+        animator.SetBool(boolName, status);
+	}
+
+	
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        
+        if(resetOnExit)
+            animator.SetBool(boolName, !status);
+	}
+
+}
